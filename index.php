04ce73400e78f795cdc1be5052d2527a1e1645e9
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:40:59 +0200

    boucles

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exo1</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $count= 0;
+while ($count <= 10) {
+ echo $count;
+ $count++;
+}
+  ?>
+</p>
+  </body>
+</html>
